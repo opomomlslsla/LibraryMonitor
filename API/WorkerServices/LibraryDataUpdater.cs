4bb7aa8dd4ee1a366a7fc0c2ded9bd9d2e9b53d5
Opomomlslsla <opomomlslsla@gmail.com>
2024-10-02 13:09:58 +0300

    fixing the previous fixes

--- API/WorkerServices/LibraryDataUpdater.cs
+++ API/WorkerServices/LibraryDataUpdater.cs
@@ -10,7 +10,7 @@ using TableDependency.SqlClient.Base.EventArgs;
 
 namespace API.WorkerServices;
 
-public sealed class LibraryDataUpdater : IDisposable
+public sealed class LibraryDataUpdater : BackgroundService
 {
     private readonly IHubContext<BooksInfoHub> _hubContext ;
     private SqlTableDependency<Book> _sqlDependency;
@@ -38,9 +38,14 @@ public sealed class LibraryDataUpdater : IDisposable
         await _hubContext.Clients.All.SendAsync("onRecieveStatistics", data);
     }
 
-    public void Dispose()
+    public override void Dispose()
     {
         _sqlDependency.Dispose();
+        base.Dispose();
     }
 
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        return Task.CompletedTask;
+    }
 }
--- Iinfrastructure/Data/Context.cs
+++ Iinfrastructure/Data/Context.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,5 +12,11 @@ namespace Iinfrastructure.Data
     public class LibraryContext(DbContextOptions<LibraryContext> options) : DbContext(options)
     {
         public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>()
+                .ToTable(tb => tb.UseSqlOutputClause(false));
+        }
     }
 }
